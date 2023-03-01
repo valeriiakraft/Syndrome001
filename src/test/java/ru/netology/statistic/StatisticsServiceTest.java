6ca493d527837d74f03dc361ef0b57446a7ad72f
Valeriia Kraft <airkraft20@gmail.com>
2023-03-01 20:38:55 +0700

    initial commit

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -10,7 +10,7 @@ public class StatisticsServiceTest {
     void findMaxWhenMaxFirst() {
         ru.netology.statistic.StatisticsService service = new ru.netology.statistic.StatisticsService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long[] incomesInBillions = {12, 5, 8, 3, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
 
         long actual = service.findMax(incomesInBillions);
